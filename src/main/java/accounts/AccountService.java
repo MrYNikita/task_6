78e1ed6ebd28727e8e88730fe79313f64ecdfeb5
Mr_Y_Nikita <Mr_Y_Nikita@gmail.com>
2021-10-12 12:55:40 +0500

    push_1

--- src/main/java/accounts/AccountService.java
+++ src/main/java/accounts/AccountService.java
@@ -1,20 +1,48 @@
 package accounts;
 
+import db.DBService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AccountService {
+    private static final DBService db;
     private static final Map<String,UserProfile> loginToProfile;
     private static final Map<String,UserProfile> sessionIdToProfile;
 
     static {
+        db = new DBService();
         loginToProfile = new HashMap<>();
         sessionIdToProfile = new HashMap<>();
 
-        loginToProfile.put("tester",new UserProfile("tester"));
+        try {
+            ResultSet rs = db.getStatement().executeQuery("select * from users");
+            while (rs.next()) {
+                loginToProfile.put(
+                        rs.getString(1),
+                        new UserProfile(
+                                rs.getString(1),
+                                rs.getString(2),
+                                rs.getString(3)
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addNewUser(UserProfile userProfile) { loginToProfile.put(userProfile.getLogin(), userProfile); }
+    public static void addNewUser(UserProfile userProfile) throws SQLException {
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+        db.getStatement().executeUpdate(
+                String.format(
+                    "insert into users (login,email,password) values ('%s','%s','%s');",
+                        userProfile.getLogin(),
+                        userProfile.getEmail(),
+                        userProfile.getPassword()));
+    }
     public static void addSession(String sessionId, UserProfile userProfile) { sessionIdToProfile.put(sessionId,userProfile); }
     public static void deleteSession(String sessionId) { sessionIdToProfile.remove(sessionId); }
 
---
+++ src/main/java/db/DBService.java
@@ -0,0 +1,45 @@
+package db;
+
+import com.mysql.cj.jdbc.Driver;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBService {
+    private final Connection connection;
+
+    public DBService() {
+        this.connection = getMysqlConnection();
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    public static Connection getMysqlConnection() {
+        try {
+            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+
+            StringBuilder url = new StringBuilder();
+
+            url.
+                    append("jdbc:mysql://").        //db type
+                    append("localhost:").           //host name
+                    append("3306/").                //port
+                    append("profiles?").          //db name
+                    append("user=tester&").          //login
+                    append("password=tester");       //password
+
+            System.out.println("URL: " + url + "\n");
+
+            return DriverManager.getConnection(url.toString());
+        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Statement getStatement() throws SQLException {
+        return connection.createStatement();
+    }
+}
